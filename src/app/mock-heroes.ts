88db69765d290754090bad6fdbbcc9f2c37883ef
tgmcgraw <tgmcgraw@fbi.gov>
2020-09-04 13:53:00 -0400

    yes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero  } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr.Nice'},
+    { id: 12, name: 'Narco'},
+    { id: 13, name: 'Mr. Incredible'},
+    { id: 14, name: 'Elasta Girl'},
+    { id: 15, name: 'Frozo'},
+    { id: 16, name: 'Iron Man'},
+    { id: 17, name: 'Hulk'},
+    { id: 18, name: 'Black Widow'},
+    { id: 19, name: 'Dr.Strange'},
+    { id: 20, name: 'Ant-Man'}
+];
